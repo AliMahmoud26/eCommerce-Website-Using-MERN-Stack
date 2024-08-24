b71e08bfbc21cbd2c2991c6797b5f233167c3fdc
AliMahmoud26 <AliMahmoudHelal26@gmail.com>
2024-08-24 20:36:02 +0300

    another editing to the bars icon

--- Frontend/src/Components/Navbar/ResponsiveNavbar.jsx
+++ Frontend/src/Components/Navbar/ResponsiveNavbar.jsx
@@ -1,90 +1,108 @@
-import React, { useState } from 'react'
-import logo from '../../assets/Frontend_Assets/shoppingLogo.png'
-import cart_icon from '../../assets/Frontend_Assets/cart_icon.png'
-import { FaBars, FaCartArrowDown, FaSearch, FaTimes, FaUser, FaUserAlt } from 'react-icons/fa';
+import React, { useState, useEffect } from 'react';
+import logo from '../../assets/Frontend_Assets/shoppingLogo.png';
+import cart_icon from '../../assets/Frontend_Assets/cart_icon.png';
+import { FaBars, FaSearch, FaTimes, FaUserAlt } from 'react-icons/fa';
 
 const ResponsiveNavbar = () => {
-    const [navbarVisible, setNavbarVisible] = useState(false);
-    const [searchVisible, setSearchVisible] = useState(false);
-    const [loginVisible, setLoginVisible] = useState(false);
+  const [navbarVisible, setNavbarVisible] = useState(false);
+  const [searchVisible, setSearchVisible] = useState(false);
+  const [loginVisible, setLoginVisible] = useState(false);
 
-    const toggleNavbar = () => setNavbarVisible(!navbarVisible);
+  const toggleNavbar = () => setNavbarVisible(!navbarVisible);
 
-    const toggleSearch = () => {
-        setSearchVisible(!searchVisible);
-        if (!searchVisible) {
-          setLoginVisible(false); // Close login holder when search is opened
-        }
-      };
-    
-      const toggleLogin = () => {
-        setLoginVisible(!loginVisible);
-        if (!loginVisible) {
-          setSearchVisible(false); // Close search bar when login holder is opened
-        }
-      };
+  const toggleSearch = () => {
+    setSearchVisible(!searchVisible);
+    if (!searchVisible) {
+      setLoginVisible(false); // Close login holder when search is opened
+    }
+  };
+
+  const toggleLogin = () => {
+    setLoginVisible(!loginVisible);
+    if (!loginVisible) {
+      setSearchVisible(false); // Close search bar when login holder is opened
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        !event.target.closest('.search-bar') && 
+        !event.target.closest('.login-page') &&
+        (searchVisible || loginVisible)
+      ) {
+        setSearchVisible(false);
+        setLoginVisible(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [searchVisible, loginVisible]);
 
   return (
     <>
-        <nav>
-            <div className="nav-header">
-                {/* Logo Component */}
-                <div className="left-nav">
-                    {/* Mobile Nav Links */}
-                    <div className="mobile-nav-links">
-                        <span className="bars-menu-icon" onClick={toggleNavbar}><FaBars /></span>
-                        <div className={`nav-links ${navbarVisible ? 'showNavbar' : ''}`}>
-                            <span className='times-icon' onClick={toggleNavbar}><FaTimes /></span>
-                            <ul className="nav-menu">
-                                <li><a href="#" className='first'><FaBars /> All Products</a></li>
-                                <li><a href="">Today's Deals</a></li>
-                                <li><a href="">Men's Fashion</a></li>
-                                <li><a href="">Women's Fashion</a></li>
-                                <li><a href="">Kids Fashion</a></li>
-                                <li><a href="">Toys & Games</a></li>
-                                <li><a href="">Health & Beauty</a></li>
-                                <li><a href="">Mobiles & Tablets</a></li>
-                                <li><a href="">TVs & Electronics</a></li>
-                                <li><a href="">Video Games</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    {/* Nav Logo */}
-                    <div className="nav-logo">
-                        <img src={logo} alt="Logo" />
-                        <a href='#'>MALLMARKT</a>
-                    </div>
-                </div>
-                <div className="right-nav">
-                    {/* Search Bar */}
-                    <div className="search-bar">
-                        <a href="#" className='search-icon' onClick={toggleSearch}><FaSearch /></a>
-                        <input type="search" name="search-bar" id="search" placeholder='Search About a Product' className={searchVisible ? 'showInput' : ''}/>
-                    </div>
-                    {/* Login Page */}
-                    <div className="login-page">
-                        <a href="#" className='login-icon' onClick={toggleLogin}><FaUserAlt /></a>
-                        <div className={`login-holder ${loginVisible ? 'showLoginHolder' : ''}`}>
-                            <a href="#">Login</a>
-                            <a href="#">Register</a>
-                        </div>
-                    </div>
-                    {/* Cart */}
-                    <div className="login-cart">
-                        <img src={cart_icon} title='Cart' alt="cart icon" />
-                        <div className="nav-cart-count">0</div>
-                    </div>
-                </div>
+      <nav>
+        <div className="nav-header">
+          {/* Left Nav Section */}
+          <div className="left-nav">
+            {/* Mobile Nav Links */}
+            <div className="mobile-nav-links">
+              <span className="bars-menu-icon" onClick={toggleNavbar}><FaBars /></span>
+              <div className={`nav-links ${navbarVisible ? 'showNavbar' : ''}`}>
+                <span className='times-icon' onClick={toggleNavbar}><FaTimes /></span>
+                <ul className="nav-menu">
+                  <li><a href="#" className='first'><FaBars /> All Products</a></li>
+                  <li><a href="">Today's Deals</a></li>
+                  <li><a href="">Men's Fashion</a></li>
+                  <li><a href="">Women's Fashion</a></li>
+                  <li><a href="">Kids Fashion</a></li>
+                  <li><a href="">Toys & Games</a></li>
+                  <li><a href="">Health & Beauty</a></li>
+                  <li><a href="">Mobiles & Tablets</a></li>
+                  <li><a href="">TVs & Electronics</a></li>
+                  <li><a href="">Video Games</a></li>
+                </ul>
+              </div>
+            </div>
+            {/* Nav Logo */}
+            <div className="nav-logo">
+              <img src={logo} alt="Logo" />
+              <a href='#'>MALLMARKT</a>
+            </div>
+          </div>
+
+          {/* Right Nav Section */}
+          <div className="right-nav">
+            {/* Search Bar */}
+            <div className="search-bar">
+              <a href="#" className='search-icon' onClick={toggleSearch}><FaSearch /></a>
+              <input type="search" name="search-bar" id="search" placeholder='Search About a Product' 
+                     className={searchVisible ? 'showInput' : ''} />
             </div>
 
-            {/* Search Bar */}
-            {/* <div className="search-bar">
-                <input type="search" name="search-bar" id="search" placeholder='Search About a Product'/>
-                <span className="search-icon"><a href="#"><FaSearch /></a></span>
-            </div> */}
-        </nav>
+            {/* Login Page */}
+            <div className="login-page">
+              <a href="#" className='login-icon' onClick={toggleLogin}><FaUserAlt /></a>
+              <div className={`login-holder ${loginVisible ? 'showLoginHolder' : ''}`}>
+                <a href="#">Login</a>
+                <a href="#">Register</a>
+              </div>
+            </div>
+
+            {/* Cart */}
+            <div className="login-cart">
+              <img src={cart_icon} title='Cart' alt="cart icon" />
+              <div className="nav-cart-count">0</div>
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
-export default ResponsiveNavbar
+export default ResponsiveNavbar;
