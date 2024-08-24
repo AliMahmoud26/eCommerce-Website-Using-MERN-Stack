71d9f35038d2f39a55b8188f6a00382c37a54c00
AliMahmoud26 <AliMahmoudHelal26@gmail.com>
2024-08-24 19:19:43 +0300

    publising the responsive navbar file and its scss style to the github repo after finishing the responsivepart of the navbar component

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,45 +1,65 @@
-import React from 'react'
-import logo from '../../assets/Frontend_Assets/shoppingLogo.png'
-import cart_icon from '../../assets/Frontend_Assets/cart_icon.png'
+import React, { useState, useEffect } from 'react';
+import logo from '../../assets/Frontend_Assets/shoppingLogo.png';
+import cart_icon from '../../assets/Frontend_Assets/cart_icon.png';
 import { FaBars, FaCartArrowDown, FaSearch } from 'react-icons/fa';
+import ResponsiveNavbar from './ResponsiveNavbar';
 
 const Navbar = () => {
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1024);
+
+  const handleResize = () => {
+    setIsDesktop(window.innerWidth > 1024);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <nav>
-        <div className="nav-header">
-          <div className="nav-logo">
-            <img src={logo} alt="Logo" />
-            <a href='#'>MALLMARKT</a>
+      {isDesktop ? (
+        <nav className='desktop-nav'>
+          <div className="nav-header">
+            <div className="nav-logo">
+              <img src={logo} alt="Logo" />
+              <a href='#'>MALLMARKT</a>
+            </div>
+            <div className="search-bar">
+              <input type="search" name="search-bar" id="search" placeholder='Search About a Product' />
+              <span className="search-icon"><a href="#"><FaSearch /></a></span>
+            </div>
+            <div className="login-cart">
+              <a href="#">Register</a>
+              <a href="#">Login</a>
+              <img src={cart_icon} title='Cart' alt="cart icon" />
+              <div className="nav-cart-count">0</div>
+            </div>
           </div>
-          <div className="search-bar">
-            <input type="search" name="search-bar" id="search" placeholder='Search About a Product'/>
-            <span className="search-icon"><a href="#"><FaSearch /></a></span>
+          <div className="nav-links">
+            <ul className="nav-menu">
+              <li><a href="#" className='first'><FaBars /> All Products</a></li>
+              <li><a href="">Today's Deals</a></li>
+              <li><a href="">Men's Fashion</a></li>
+              <li><a href="">Women's Fashion</a></li>
+              <li><a href="">Kids Fashion</a></li>
+              <li><a href="">Toys & Games</a></li>
+              <li><a href="">Health & Beauty</a></li>
+              <li><a href="">Mobiles & Tablets</a></li>
+              <li><a href="">TVs & Electronics</a></li>
+              <li><a href="">Video Games</a></li>
+            </ul>
           </div>
-          <div className="login-cart">
-            <a href="#">Register</a>
-            <a href="#">Login</a>
-            <img src={cart_icon} title='Cart' alt="cart icon" />
-            <div className="nav-cart-count">0</div>
-          </div>
+        </nav>
+      ) : (
+        <div className='mobile-nav'>
+          <ResponsiveNavbar />
         </div>
-        <div className="nav-links">
-          <ul className="nav-menu">
-            <li><a href="#" className='first'><FaBars /> All Products</a></li>
-            <li><a href="">Today's Deals</a></li>
-            <li><a href="">Men's Fashion</a></li>
-            <li><a href="">Women's Fashion</a></li>
-            <li><a href="">Kids Fashion</a></li>
-            <li><a href="">Toys & Games</a></li>
-            <li><a href="">Health & Beauty</a></li>
-            <li><a href="">Mobiles & Tablets</a></li>
-            <li><a href="">TVs & Electronics</a></li>
-            <li><a href="">Video Games</a></li>
-          </ul>
-        </div>
-      </nav>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
